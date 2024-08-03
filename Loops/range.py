97a1a946ef831b1ee384ebecbf7bd04dd912ad94
Ansh <159460386+HiGautamAG@users.noreply.github.com>
2024-08-03 19:34:51 +0530

    Update range.py

--- Loops/range.py
+++ Loops/range.py
@@ -22,8 +22,13 @@ for i  in range (2,100, 2): #print even number from 2 to 100
 for i in range(1, 100, 2): #print odd number from 1 to 100
     print(i)
     
+for i in range(100, 1, -1): #print number from 100 to 1
+    print(i)
     
+    n = int(input("Enter the number: "))
     
+    for i in range(1, 11):
+        print(n*i)    
             
             
             
